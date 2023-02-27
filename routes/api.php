08abe42f776d783b70fa0ff26ffd128c655738b8
Leandro Parice <contato@leandroparice.com.br>
2023-02-27 19:13:41 -0300

    api teste

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,28 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/login', function(Request $request){
+    // $request->validate([
+    //     'email' => 'required|email',
+    //     'password' => 'required',
+    //     'device_name' => 'required',
+    // ]);
+ 
+    $user = User::where('email', 'olaf.koss@example.org')->first();
+ 
+    if (! $user || ! Hash::check('password', $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken('insomnia')->plainTextToken;
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
